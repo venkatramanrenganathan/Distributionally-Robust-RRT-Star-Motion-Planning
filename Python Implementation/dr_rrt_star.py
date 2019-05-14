b1413342dcaedcc693ffbb13795aaf27dcc6529b
Renganathan <vxr131730@utdallas.edu>
2019-05-13 19:18:20 -0500

    Initial Commit

--- Python Implementation/dr_rrt_star.py
+++ Python Implementation/dr_rrt_star.py
@@ -10,8 +10,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 from matplotlib.patches import Rectangle
+from scipy.linalg import block_diag
+from numpy.linalg import inv
 
-show_animation = True
+
+# Global Variables
+show_animation = True  # Flag to decide to show animation or not
+STEER_TIME     = 10.0  # Maximum Steering Time Horizon
+DT             = 0.1   # Time tick(discretization time)
 
 
 class RRT():
@@ -19,7 +25,7 @@ class RRT():
     Class for RRT Planning
     """
 
-    def __init__(self, start, goal, obstacleList, randArea,
+    def __init__(self, start, goal, obstacleList, init_param, randArea,
                  expandDis=0.5, goalSampleRate=20, maxIter=300):
         """
         Setting Parameter
@@ -28,27 +34,18 @@ class RRT():
         obstacleList:obstacle Positions [[x,y,size],...]
         randArea:Ramdom Samping Area [min,max]
         """
-        self.start = Node(start[0], start[1])  # Start Node Coordinates
-        self.end = Node(goal[0], goal[1])      # Goal Node Coordinates
-        self.minrand = randArea[0]
-        self.maxrand = randArea[1]
-        self.expandDis = expandDis
+        self.start          = Node(start[0], start[1])  # Start Node Coordinates
+        self.end            = Node(goal[0], goal[1])      # Goal Node Coordinates
+        self.minrand        = randArea[0]
+        self.maxrand        = randArea[1]
+        self.expandDis      = expandDis
         self.goalSampleRate = goalSampleRate
-        self.maxIter = maxIter
-        self.obstacleList = obstacleList   
+        self.maxIter        = maxIter
+        self.obstacleList   = obstacleList 
+        self.start.covar    = init_param[8]
         
-         # Double Integrator Data    
-        self.dt   = 0.1                                                       # Time Discretization Period 
-        self.A    = np.array([[1,0,dt,0],[0, 1, 0, dt],[0,0,1,0],[0,0,0,1]])  # Dynamics Matrix
-        self.B    = np.array([[(dt**2)/2, 0],[0, (dt**2)/2],[dt, 0],[0,dt]])  # Input Matrix
-        self.C    = np.array([[1,0,0,0],[0,1,0,0]])                           # Output Matrix
-        self.G    = B                                                         # Disturbance Input Matrix
-        self.Q    = np.array([[4*np.zeros((2,2)), np.zeros((2,2))],[np.zeros((2,2)), 0.1*np.identity(2)]])           # State Stage cost Penalty
-        self.QT   = np.array([[100*np.zeros((2,2)), np.zeros((2,2))],[np.zeros((2,2)), 0.1*np.identity(2)]])         # State Terminal Penalty
-        self.R    = 0.02*np.identity(2)                                                                              # Input Penalty 
-        self.W    = np.array([[np.zeros((2,2)), np.zeros((2,2))],[np.zeros((2,2)), 0.001*np.array([[2,1],[1,2]])]])  # Disturbance covariance
-        self.Th   = 1                                                                                                # Steering Time Horizon
-        self.S0   = np.array([[0.001*np.zeros((2,2)), np.zeros((2,2))],[np.zeros((2,2)), np.zeros((2,2))]])          # Initial State Covariance
+        # Double Integrator Data    
+        self.init_param   = init_param       
         
 
     def Planning(self, animation=True):
@@ -58,11 +55,12 @@ class RRT():
         """
 
         self.nodeList = [self.start]
+        
         for i in range(self.maxIter):            
             rnd = self.get_random_point()
             nind = self.GetNearestListIndex(self.nodeList, rnd)
 
-            newNode = self.steer(rnd, nind)
+            newNode = self.steer(rnd, nind, self.init_param)
             #  print(newNode.cost)
 
             if self.__CollisionCheck(newNode, self.obstacleList):
@@ -104,28 +102,95 @@ class RRT():
             print("mincost is inf")
             return newNode
 
-        newNode.cost = mincost
+        newNode.cost   = mincost
         newNode.parent = minind
 
         return newNode
+    
+    def LQGplanning(self, from_node, to_node, init_param):        
+    
+        # Linear system model
+        A = init_param[0]
+        B = init_param[1]
+        C = init_param[2]
+        Q = init_param[3]       
+        R = init_param[4]
+        W = init_param[5]
+        G = init_param[6]
+        QT = init_param[7]
+        
+        n,m = np.shape(B)
+        T   = STEER_TIME
+        rx, ry = [from_node.x], [from_node.y]
+        tonode = np.array([to_node.x, to_node.y,0,0])
+        x = np.array([from_node.x - to_node.x, from_node.y - to_node.y, 0 , 0]).reshape(n, 1)  # State vector
+        
+        # Run dynamic programming to compute optimal controller
+        K = np.zeros((m,n,T))
+        k = np.zeros((m,T))
+        P = np.zeros((n,n,T+1))
+        p = np.zeros((n,T+1)) 
+    
+        P[:,:,T+1] = QT
+        p[:,T+1]   = -np.dot(QT,tonode)
 
-    def steer(self, rnd, nind):
+        for t in range(T,0,-1):
+            P[:,:,t] = Q + A.T @ P[:,:,t+1]@ A - A.T @ P[:,:,t+1] @ B @ inv(R+B.T @ P[:,:,t+1] @ B) @ B.T @ P[:,:,t+1] @ A
+            K[:,:,t] = -inv(R+B.T @ P[:,:,t+1]@B) @ B.T @ P[:,:,t+1]@A
+            k[:,t]   = -inv(R+B.T@P[:,:,t+1]@B)@ B.T @ p[:,t+1]
+            p[:,t]   = A.T@p[:,t+1]- np.dot(Q,tonode) + K[:,:,t].T @ B.T @ p[:,t+1] + transpose(A) @ P[:,:,t+1] @ B @ k[:,t] + K[:,:,t].T @ (R+B.T@P[:,:,t+1]@B) @ k[:,t] 
+        
+        # Preallocate matrices
+        x          = np.zeros((n,T+1));
+        u          = np.zeros((m,T));
+        x[:,1]     = tonode;
+        V          = np.identity(n)
+        C          = np.identity(n)
+        H          = np.identity(n)
+        G          = np.identity(n)
+        P_x0       = np.zeros((n))
+        P_x_est_0  = np.zeros((n))
+        pi_0       = block_diag(P_x0, P_x_est_0) # Joint Covariance of true and estimated states
+        Sigma_V    = np.zeros((n,n,T))
+        S          = np.zeros((n,n,T+1))
+        x_est      = np.zeros((n,T+1))
+        x_est[:,1] = tonode                      # Estimated State
+        KG         = np.zeros(n,n,T+1)           # Kalman Gain
+        S[:,:,1]   = from_node.covar 
+        A_bar      = np.zeros((2*n,2*n,T))
+        pi         = np.zeros((2*n,2*n,T+1))
+        pi[:,:,1]  = pi_0
+        
+        # Steer the robot across the finite time horizon using LQG control
+        for t in range(1,T+1):
+            # Realize the measurement noise
+            Sigma_v      = 0.001*np.identity(n)
+            # control uses estimated state
+            u[:,t]       = K[:,:,t] @ x_est[:,t] + k[:,t] 
+            # Update the true state
+            x[:,t+1]     = A @ x[:,t] + B @ u[:,t]
+            # update the kalmann gain
+            KG[:,:,t]    = S[:,:,t] @ C.T @ inv(C @ S[:,:,t] @ C.T + H @ Sigma_V @ H.T)
+            # update the estimated state
+            x_est[:,t+1] = KG[:,:,t] @ C @ A @ x[:,t] + (np.identity(n) - KG[:,:,t]@C) @ A @ x_est[:,t] + B @ u[:,t]
+            # stack up the true and estimated states
+            A_bar[:,:,t] = np.array([[A, B @ K[:,:,t]], [KG[:,:,t]@C@A, (np.identity(n)-KG[:,:,t]@C)@A+B@K[:,:,t]]])
+            B_bar[:,:,t] = np.array([[B],[B]])
+            G_bar[:,:,t] = np.array([[G, np.zeros((n,n))], [KG[:,:,t]@C@G, KG[:,:,t]@H]])
+            # propagate the joint covariance
+            pi[:,:,t+1]  = A_bar[:,:,t]@pi[:,:,t]@A_bar[:,:,t].T + G_bar[:,:,t] @ block_diag(W, Sigma_V) @ G_bar[:,:,t].T
+            # Extract the true state covariance alone
+            S[:,:,t+1]   = np.array([np.identity(n), np.zeros((n,n))]) @ pi[:,:,t+1] @  np.array([np.identity(n), np.zeros((n,n))]).T 
+            
+        return x, S
 
+    def steer(self, rnd, nind, init_param):
+        
         # expand tree
-        nearestNode = self.nodeList[nind]
-        theta = math.atan2(rnd[1] - nearestNode.y, rnd[0] - nearestNode.x)
-        newNode = Node(rnd[0], rnd[1])
-        currentDistance = math.sqrt(
-            (rnd[1] - nearestNode.y) ** 2 + (rnd[0] - nearestNode.x) ** 2)
-        # Find a point within expandDis of nind, and closest to rnd
-        if currentDistance <= self.expandDis:
-            pass
-        else:
-            newNode.x = nearestNode.x + self.expandDis * math.cos(theta)
-            newNode.y = nearestNode.y + self.expandDis * math.sin(theta)
-        newNode.cost = float("inf")
-        newNode.parent = None
-        return newNode
+        nearestNode = self.nodeList[nind]                
+        x, S = self.LQGplanning(nearestNode, rnd, init_param)
+        
+        return x, S
 
     def get_random_point(self):
 
@@ -249,13 +314,26 @@ class Node():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.covar  = np.zeros((4,4))
         self.cost   = 0.0
         self.parent = None
-        self.covar  = np.zeros((4,4))
+        
 
 
 def main():
     print("Start " + __file__)
+    
+    # Double Integrator Data                               
+    A  = np.array([[1,0,DT,0],[0, 1, 0, DT],[0,0,1,0],[0,0,0,1]])  # Dynamics Matrix
+    B  = np.array([[(DT**2)/2, 0],[0, (DT**2)/2],[DT, 0],[0,DT]])  # Input Matrix
+    C  = np.array([[1,0,0,0],[0,1,0,0]])                           # Output Matrix
+    G  = B                                                         # Disturbance Input Matrix
+    Q  = np.array([[4*np.zeros((2,2)), np.zeros((2,2))],[np.zeros((2,2)), 0.1*np.identity(2)]])           # State Stage cost Penalty
+    QT = np.array([[100*np.zeros((2,2)), np.zeros((2,2))],[np.zeros((2,2)), 0.1*np.identity(2)]])         # State Terminal Penalty
+    R  = 0.02*np.identity(2)                                                                              # Input Penalty 
+    W  = np.array([[np.zeros((2,2)), np.zeros((2,2))],[np.zeros((2,2)), 0.001*np.array([[2,1],[1,2]])]])  # Disturbance covariance    
+    S0 = np.array([[0.001*np.zeros((2,2)), np.zeros((2,2))],[np.zeros((2,2)), np.zeros((2,2))]])          # Initial State Covariance
+    init_param = [A,B,C,G,Q,QT,R,W,S0]
 
     # ====Search Path with RRT====
     obstacleList = [
@@ -268,7 +346,7 @@ def main():
     ]  # Obstacle Location Format [ox,oy,wd,ht]- ox, oy specifies the bottom left corner of rectangle with width: wd and height: ht.]
 
     # Set Initial parameters
-    rrt  = RRT(start=[0, 0], goal=[7, 9], randArea=[-2, 15], obstacleList=obstacleList)
+    rrt  = RRT(start=[0, 0], goal=[7, 9], randArea=[-2, 15], obstacleList=obstacleList, init_param=init_param)
     path = rrt.Planning(animation=show_animation)
 
     if path is None:
@@ -284,8 +362,7 @@ def main():
             plt.pause(0.01)  # Need for Mac
             ells = [Ellipse(xy=[x, y],
                 width=np.random.rand(), height=np.random.rand(),
-                angle=np.random.rand() * 360) for (x, y) in path]
-            ax = plt.axes()                      
+                angle=np.random.rand() * 360) for (x, y) in path]                              
             for e in ells:
                 plt.axes().add_artist(e)
                 e.set_clip_box(plt.axes().bbox)
